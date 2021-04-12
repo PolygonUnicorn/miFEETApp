d119d9dc31773018cd7ab217cd754764c7838255
Polygon Unicorn <26953790+PolygonUnicorn@users.noreply.github.com>
2021-04-12 11:31:14 +0200

    Add text justify functionality
    Added methods for the buttons that justify text
    
    Took 17 minutes

--- app/js/functions_text_format.js
+++ app/js/functions_text_format.js
@@ -2,15 +2,30 @@ module.exports = {
     /**
      * Makes the selected text bold
      */
-    makeSelectedBold: () => document.execCommand('bold'),
+    makeBold: () => document.execCommand('bold'),
 
     /**
      * Makes the selected text underlined
      */
-    makeSelectedUnderline: () => document.execCommand('underline'),
+    makeUnderline: () => document.execCommand('underline'),
 
     /**
      * Makes the selected text italic
      */
-    makeSelectedItalics: () => document.execCommand('italic'),
+    makeItalics: () => document.execCommand('italic'),
+
+    /**
+     * Align text left
+     */
+    justifyLeft: () => document.execCommand('justifyLeft'),
+
+    /**
+     * Align text center
+     */
+    justifyCenter: () => document.execCommand('justifyCenter'),
+
+    /**
+     * Align text right
+     */
+    justifyRight: () => document.execCommand('justifyRight'),
 }
